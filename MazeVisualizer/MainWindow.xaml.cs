93ac32c588153419f0c31df18e688affca2c0566
Sugawara <g15219@g.ichinoseki.ac.jp>
2019-03-26 21:27:17 +0900

    Update : 迷路のマスを描画
    Mazeクラスを作成し迷路のマスを描画するメソッドなどを追加。

--- MazeVisualizer/MainWindow.xaml.cs
+++ MazeVisualizer/MainWindow.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MazeVisualizer
 {
-    /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
+        /// <summary>
+        /// MainWindow.xaml の相互作用ロジック
+        /// </summary>
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
+            public MainWindow()
+            {
+                InitializeComponent();
+                Maze m = new Maze(16, 16, 17, 17);
+                m.drawGrid(maze);
+            }
         }
-    }
 }
---
+++ MazeVisualizer/Maze.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MazeVisualizer
+{
+    class Maze
+    {
+        /* プロパティ */
+        private int grid_height { get; set; }   // 1マスの縦幅
+        private int grid_width { get; set; }    // 1マスの横幅
+        private int grid_row { get; set; }      // 行の数
+        private int grid_column { get; set; }   // 列の数
+
+        /* コンストラクタ */
+        public Maze(int height, int width, int row, int column)
+        {
+            Grid_Height = height;
+            Grid_Width = width;
+            Grid_Row = row;
+            Grid_Column = column;
+        }
+
+        /* プロパティのアクセサ */
+        public int Grid_Height
+        {
+            get
+            {
+                return grid_height;
+            }
+            set
+            {
+                grid_height = value;
+            }
+        }
+
+        public int Grid_Width
+        {
+            get
+            {
+                return grid_width;
+            }
+            set
+            {
+                grid_width = value;
+            }
+        }
+
+        public int Grid_Row
+        {
+            get
+            {
+                return grid_row;
+            }
+            set
+            {
+                grid_row = value;
+            }
+        }
+
+        public int Grid_Column
+        {
+            get
+            {
+                return grid_column;
+            }
+            set
+            {
+                grid_column = value;
+            }
+        }
+
+        /* 迷路のマス目を描画する */
+        public void drawGrid(Canvas target_canvas)
+        {
+            Rectangle[,] grid = new Rectangle[grid_row, grid_column];
+
+            for (int i = 0; i < grid_row; i++)
+            {
+                for (int j = 0; j < grid_column; j++)
+                {
+                    grid[i, j] = new Rectangle();
+                    grid[i, j].Stroke = Brushes.Black;
+                    grid[i, j].Fill = Brushes.White;
+                    grid[i, j].Height = grid_height;
+                    grid[i, j].Width = grid_width;
+                    Canvas.SetTop(grid[i, j], i * (grid_height - 1));
+                    Canvas.SetLeft(grid[i, j], j * (grid_width - 1));
+                    target_canvas.Children.Add(grid[i, j]);
+                }
+            }
+        }
+    }
+}
