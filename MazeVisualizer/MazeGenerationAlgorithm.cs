2ccb86a429267d0b0df128fd59e276aea0457f3f
Sugawara <g15219@g.ichinoseki.ac.jp>
2019-04-01 21:35:09 +0900

    Update : 壁伸ばし法を移植
    壁伸ばし法をMazeGenerationAlgorithmに移植し、処理の内容を変更。

--- MazeVisualizer/MazeGenerationAlgorithm.cs
+++ MazeVisualizer/MazeGenerationAlgorithm.cs
@@ -64,7 +64,89 @@ namespace MazeVisualizer
 
         // 壁伸ばし法
         public void WallExtendMethod(Maze maze)
-        { 
+        {
+            Random rand = new Random();
+            Stack<Maze.Pillar> prev_pillar_coord = new Stack<Maze.Pillar>();
+
+            while (maze.PillarCoordinate.Count != 0)
+            {
+                Maze.Pillar coord = maze.PillarCoordinate[rand.Next(maze.PillarCoordinate.Count)];
+                int x = coord.X, y = coord.Y;
+                maze.PillarCoordinate.Remove(new Maze.Pillar { X = x, Y = y });
+
+                if (!maze.IsWall[y, x])
+                {
+                    maze.IsWall[y, x] = true;
+
+                    while (true)
+                    {
+                        List<Direction> wall_candidates_direction = new List<Direction>();
+                        if (!maze.IsWall[y - 2, x] && !prev_pillar_coord.Contains(new Maze.Pillar { X = x, Y = y - 2 }))
+                        {
+                            wall_candidates_direction.Add(Direction.UP);
+                        }
+                        if (!maze.IsWall[y + 2, x] && !prev_pillar_coord.Contains(new Maze.Pillar { X = x, Y = y + 2 }))
+                        {
+                            wall_candidates_direction.Add(Direction.DOWN);
+                        }
+                        if (!maze.IsWall[y, x - 2] && !prev_pillar_coord.Contains(new Maze.Pillar { X = x - 2, Y = y }))
+                        {
+                            wall_candidates_direction.Add(Direction.LEFT);
+                        }
+                        if (!maze.IsWall[y, x + 2] && !prev_pillar_coord.Contains(new Maze.Pillar { X = x + 2, Y = y }))
+                        {
+                            wall_candidates_direction.Add(Direction.RIGHT);
+                        }
+
+                        if (wall_candidates_direction.Count == 0)
+                        {
+                            Maze.Pillar next_coord = prev_pillar_coord.Pop();
+                            x = next_coord.X;
+                            y = next_coord.Y;
+                        }
+                        else
+                        {
+                            int x_moving_distance = 0, y_moving_distance = 0;
+                            int wall_direction = rand.Next(wall_candidates_direction.Count);
+
+                            switch (wall_direction)
+                            {
+                                case 0:
+                                    y_moving_distance = -1;
+                                    break;
+                                case 1:
+                                    y_moving_distance = 1;
+                                    break;
+                                case 2:
+                                    x_moving_distance = -1;
+                                    break;
+                                case 3:
+                                    x_moving_distance = 1;
+                                    break;
+                            }
+
+                            if (!prev_pillar_coord.Contains(new Maze.Pillar { X = x + 2 * x_moving_distance, Y = y + 2 * y_moving_distance }))
+                            {
+                                maze.IsWall[y + y_moving_distance, x + x_moving_distance] = true;
+
+                                break;
+                            }
+                            else
+                            {
+                                for (int i = 1; i <= 2; i++)
+                                {
+                                    maze.IsWall[y + i * y_moving_distance, x + i * x_moving_distance] = true;
+                                }
+                                prev_pillar_coord.Push(new Maze.Pillar { X = x, Y = y });
+                                x += 2 * x_moving_distance;
+                                y += 2 * y_moving_distance;
+                                maze.PillarCoordinate.Remove(new Maze.Pillar { X = x, Y = y });
+                            }
+                        }
+                    }
+                    prev_pillar_coord.Clear();
+                }
+            }
         }
 
         public void DiggingMethod()
